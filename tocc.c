cedbd866ab3ca12cfebfd9c59dbcdbe6135f427b
fukuseki <okubo.ho.ne.jp@gmail.com>
2019-08-17 14:17:04 +0900

    抽象構文木とそれを使ったコードジェネレータを導入

--- tocc.c
+++ tocc.c
@@ -126,38 +126,99 @@ Token* tokenize(char* p) {
   return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD,  // +
+  ND_SUB,  // -
+  ND_NUM,  // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind;  // ノードの種類
+  Node* lhs;      // 左辺
+  Node* rhs;      // 右辺
+  int val;        // kindがND_NUMの場合のみ使う
+};
+
+Node* new_node(NodeKind kind, Node* lhs, Node* rhs) {
+  Node* node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node* new_node_num(int val) {
+  Node* node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node* expr() {
+  Node* node = new_node_num(expect_number());
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, new_node_num(expect_number()));
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, new_node_num(expect_number()));
+    } else {
+      return node;
+    }
+  }
+}
+
+void gen(Node* node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    default:
+      error("予期しないkind=%d", node->kind);
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
   }
 
-  // 入力プログラムを保持
+  // トークナイズしてパースする
   user_input = argv[1];
-
-  // トークナイズする
   token = tokenize(user_input);
+  Node* node = expr();
 
   // アセンブリの先頭部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  // 抽象構文木を下りながらコード生成
+  gen(node);
 
-  // `+ <数>` または `- <数>` というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数全体の返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
