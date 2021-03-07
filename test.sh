88f41569d584aee4f4322721e9e4d01973de25e3
fukuseki <50761648+fukuseki@users.noreply.github.com>
2021-03-07 10:58:46 +0900

    メモリリークチェックが誤って有効だったため無効に

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-ASAN_OPTIONS=detect_leaks=1 ./tocc test/test.c > tmp.s
+ASAN_OPTIONS=detect_leaks=0 ./tocc test/test.c > tmp.s
 arm-linux-gnueabihf-gcc -static -g -use-ld=gold -o tmp tmp.s foo.o
 qemu-arm ./tmp
